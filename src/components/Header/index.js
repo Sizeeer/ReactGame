72e6576fe0faac97b8588ab5dda0151c45a8fb8c
Maksim <igrekzero@gmail.com>
2022-04-16 13:02:39 +0300

    add characters cards to main page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 import logoSrc from "../../assets/logo.png";
 import { Container } from "../Container";
 
--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
-import styles from "./Heading.module.css";
+import styles from "./Heading.module.scss";
 
-const Heading = ({ children, className, black = false, level = 1 }) => {
+const Heading = ({
+  children,
+  className,
+  black = false,
+  level = 1,
+  backLine = false,
+}) => {
   return React.createElement(
     `h${level}`,
     {
       className: cn(className, styles.level, styles[`level${level}`], {
         [styles.colorBlack]: black,
+        [styles.backline]: backLine,
       }),
     },
     children
@@ -20,6 +27,7 @@ Heading.propTypes = {
   level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
   black: PropTypes.bool,
   className: PropTypes.string,
+  backLine: PropTypes.bool,
 };
 
 export { Heading };

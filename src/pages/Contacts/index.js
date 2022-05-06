809e70c483c9027163c3bcf1699433b8319c9daa
Maksim <igrekzero@gmail.com>
2022-05-06 15:50:28 +0300

    finish homework_5

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Container } from "../../components/Container";
-import avatar from "assets/avatar.jpg";
+import avatar from "./../../assets/avatar.jpg";
+import { Text } from "../../components/Text";
+import styles from "./Contacts.module.scss";
 
-export const index = () => {
+export const Contacts = () => {
   return (
-    <Container>
+    <Container className={styles.root}>
       <img src={avatar} alt="avatar" />
+      <div className={styles.infoWrapper}>
+        <Text element="h2">Крисанов Максим</Text>
+        <Text element="h4">Full-stack разработчик)</Text>
+        <Text element="h4">
+          Основной стек: react, rtk/redux, react-query, react-hook-form,
+          material-ui, nodejs, expressjs, express-validator, sequelize
+        </Text>
+      </div>
     </Container>
   );
 };

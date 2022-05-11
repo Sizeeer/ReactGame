e9ae359113e77521f34d591f800b802c521010d6
Maksim <igrekzero@gmail.com>
2022-05-11 15:58:30 +0300

    finish homework_6

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useMatch } from "react-router-dom";
 import styles from "./Layout.module.scss";
 import { Container } from "../Container";
 
 export const Layout = () => {
+  const { hash, pathname } = useLocation();
+  const onMainPage = useMatch({ path: "/" });
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const load = () => {
+      setIsLoaded(true);
+    };
+
+    window.addEventListener("load", load);
+
+    return () => {
+      window.removeEventListener("load", load);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isLoaded) {
+      if (hash === "") {
+        window.scrollTo(0, 0);
+      } else {
+        const anchorElement = document.querySelector(`a[href*="${hash}"]`);
+        if (anchorElement) {
+          setTimeout(() => {
+            anchorElement.scrollIntoView({
+              block: "center",
+              behavior: "smooth",
+            });
+          }, 0);
+        }
+      }
+    }
+  }, [hash, pathname, isLoaded]);
+
   return (
     <>
       <Header />
-      <div className={styles.container}>
-        <Container>
-          <Outlet />
-        </Container>
-      </div>
+      {onMainPage ? (
+        <Outlet />
+      ) : (
+        <div className={styles.container}>
+          <Container>
+            <Outlet />
+          </Container>
+        </div>
+      )}
+
       <Footer />
     </>
   );
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "../../components/Container";
+import { Heading } from "../../components/Heading";
+
+export const NotFound = () => {
+  return (
+    <Container>
+      <Heading level={1}>NotFound</Heading>
+    </Container>
+  );
+};
